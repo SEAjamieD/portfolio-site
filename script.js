a94462aaa9971b54342f135a2631490666b8b931
Jamie Degnan <jamie.degnan@gmail.com>
2017-08-24 10:01:40 -0700

    add fade in and remove slide down

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 $(document).ready(function() {
+
+  //Slide out nav menu
   $navButton = $('.nav-menu-placeholder');
   $navButton.on('click', function(){
+    $('.nav ul').toggleClass('hide')
     $('.nav-wrap').toggleClass('show-nav');
+    $('.nav ul').fadeIn(1000);
+    // $('.nav ul a').fadeIn(1000);
   });
 
-  $('.nav ul a').on('click', function (){
+  $('.nav ul a').on('click', function(){
     $('.nav-wrap').toggleClass('show-nav');
+    // $('.nav ul').toggleClass('hide')
+    $('.nav ul').fadeOut(1000);
   });
 
 });
