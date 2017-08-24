be9b5357fb6f7eafd72f501316b3ce4f0835a791
Jamie Degnan <jamie.degnan@gmail.com>
2017-08-24 10:25:07 -0700

    Change toggleClass to addClass

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ $(document).ready(function() {
   //Slide out nav menu
   $navButton = $('.nav-menu-placeholder');
   $navButton.on('click', function(){
-    $('.nav ul').toggleClass('hide')
+    $('.nav ul').addClass('hide')
     $('.nav-wrap').toggleClass('show-nav');
     $('.nav ul').fadeIn(1000);
-    // $('.nav ul a').fadeIn(1000);
   });
 
   $('.nav ul a').on('click', function(){
     $('.nav-wrap').toggleClass('show-nav');
-    // $('.nav ul').toggleClass('hide')
     $('.nav ul').fadeOut(1000);
   });
 
